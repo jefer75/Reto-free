20a6da25d3cfb4a333456fabda5b823473aea04c
Juanxho24 <159932570+Juanxho24@users.noreply.github.com>
2024-04-29 01:04:09 -0500

    Update mapas.php

--- model/usuario/consultar/mapas.php
+++ model/usuario/consultar/mapas.php
@@ -36,7 +36,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../../css/mapas.css">
+    <link rel="stylesheet" href="../../../css/mapas2.css">
     <title>Mapas</title>
 </head>
 <body>
